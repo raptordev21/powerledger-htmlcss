12a4548a0ae17a5b6293447e4ca96e10ec1432c9
Himanshu Choudhary <raptordev21@gmail.com>
2021-07-31 13:40:29 +0530

    added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
-let map;
-let loc = { lat: 28.6712, lng: 77.3177 }
-let marker;
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
+    let loc = { lat: 28.6712, lng: 77.3177 }
+
+    let map = new google.maps.Map(document.getElementById("map"), {
         center: loc,
         zoom: 9,
     });
 
-    marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         position: loc,
         map: map
     });
 }
+
+// Sticky menu background
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9
+    } else {
+        document.querySelector('#navbar').style.opacity = 1
+    }
+})
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800
+        );
+    }
+});
+
+// -100 to change scroll position
+// scrollTop: $(hash).offset().top - 100
